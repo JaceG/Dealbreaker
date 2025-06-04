acc422be170868f2fe9d99d85155876a28aa55ce
Jace Galloway <jace.galloway@gmail.com>
2025-06-03 22:40:55 -0400

    added Profile creation screen and functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,48 +1,65 @@
-import { router, Tabs } from "expo-router";
-import { FontAwesome } from "@expo/vector-icons";
-import Feather from "@expo/vector-icons/Feather";
-import { Button } from "react-native";
-import * as SecureStore from "expo-secure-store";
+import { router, Tabs } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import Feather from '@expo/vector-icons/Feather';
+import { Button } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 const Layout = () => {
-  async function clearSecureStore() {
-    const keys = ["authToken", "userData"];
-    for (const key of keys) {
-      await SecureStore.deleteItemAsync(key);
-    }
-  }
+	async function clearSecureStore() {
+		const keys = ['authToken', 'userData'];
+		for (const key of keys) {
+			await SecureStore.deleteItemAsync(key);
+		}
+	}
 
-  const logout = async () => {
-    await clearSecureStore();
-    router.replace("/login");
-  };
+	const logout = async () => {
+		await clearSecureStore();
+		router.replace('/login');
+	};
 
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="home" color={color} size={28} />
-          ),
-          headerRight: () => <Button title="Logout" onPress={logout} />,
-        }}
-      />
-      <Tabs.Screen
-        name="create-flag/index"
-        options={{
-          headerTitle: "Create Flag",
-          title: "Create Flag",
-          tabBarIcon: ({ color }) => (
-            <Feather name="flag" size={28} color={color} />
-          ),
-          headerRight: () => <Button title="Logout" onPress={logout} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs>
+			<Tabs.Screen
+				name='index'
+				options={{
+					headerTitle: 'Home',
+					title: 'Home',
+					tabBarIcon: ({ color }) => (
+						<FontAwesome name='home' color={color} size={28} />
+					),
+					headerRight: () => (
+						<Button title='Logout' onPress={logout} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='create-flag/index'
+				options={{
+					headerTitle: 'Create Flag',
+					title: 'Create Flag',
+					tabBarIcon: ({ color }) => (
+						<Feather name='flag' size={28} color={color} />
+					),
+					headerRight: () => (
+						<Button title='Logout' onPress={logout} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name='create-profile/index'
+				options={{
+					headerTitle: 'Create Profile',
+					title: 'Create Profile',
+					tabBarIcon: ({ color }) => (
+						<Feather name='user' size={28} color={color} />
+					),
+					headerRight: () => (
+						<Button title='Logout' onPress={logout} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
 };
 
 export default Layout;
